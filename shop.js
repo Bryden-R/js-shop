42c6e3f63f122798200a306d1b7f39638197d3ad
Bryden.R <bryden325@gmail.com>
2024-09-10 16:33:36 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,42 +1,35 @@
-// products and priceing
-const products = ["apple", "banana", "orange"];
+const products = ["Apple", "Banana", "Orange"];
 const prices = [6, 9, 7];
 
-// Display available products and their prices
+//products and prices
 function displayProducts() {
-    console.log("Products available:");
-    for (let i = 0; i < products.length; i++) {
-        console.log(`${i + 1}. ${products[i]} - R${prices[i]}`);
-    }
+  console.log("Available Products:");
+  for (let i = 0; i < products.length; i++) {
+    console.log(`${i + 1}. ${products[i]} - R${prices[i]}`);
+  }
 }
 
-// Calculation of total products
-function calculateTotal(selectedProducts) {
-    let total = 0;
-    for (let i = 0; i < selectedProducts.length; i++) {
-        total += prices[selectedProducts[i] - 1]; 
-    }
-    return total;
+function calculateTotalCost(selectedItems) {
+  let totalCost = 0;
+  for (let i = 0; i < selectedItems.length; i++) {
+    totalCost += prices[selectedItems[i]];
+  }
+  return totalCost;
 }
 
-// purchasing products
-function runShopSimulation() {
-    const budget = 50;
-    const selectedProducts = [1, 2]; 
-
-    displayProducts();
-
-    //total cost
-    let totalCost = calculateTotal(selectedProducts);
-    
-    // Check if the buye can afford
-    if (totalCost > budget) {
-        console.log("You don't have enough money.");
-    } else {
-        let change = budget - totalCost;
-        console.log(`Total cost: R${totalCost}`);
-        console.log(`Remaining change: R${change}`);
-    }
+// to buy
+function shop(budget) {
+  displayProducts();
+  let selectedItems = [0, 1]; // selected products
+  let totalCost = calculateTotalCost(selectedItems);
+  if (totalCost > budget) {
+    console.log("Sorry, Insufficient Funds.");
+  } else {
+    let change = budget - totalCost;
+    console.log(`Total cost: R${totalCost}`);
+    console.log(`Change Remaining: R${change}`);
+  }
 }
 
-
+let budget = 50;
+shop(budget);
